daf07121b937412c31ba46f016ba7a5325ca73c0
MiguelAngelAlvarezFernandez <mayprofprog@gmail.com>
2023-02-03 20:47:53 +0100

    Primer heading desde JS

--- Prueba.js
+++ Prueba.js
@@ -4,3 +4,10 @@ let MiPrimerParrafo
 MiPrimerParrafo = document.createElement("p")
 MiPrimerParrafo.innerText = "Lo que hay que hacer para poner una frase"
 ElBody.appendChild(MiPrimerParrafo)
+
+let MiprimerHeading
+MiprimerHeading = document.createElement("h2")
+MiprimerHeading.innerText = "A ver si sale mi primer heading"
+ElBody.appendChild(MiprimerHeading)
+
+console.log(MiprimerHeading)
