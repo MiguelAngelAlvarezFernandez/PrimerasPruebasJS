f93b0673950126b9c8afcc72ed7b01bfce287983
MiguelAngelAlvarezFernandez <mayprofprog@gmail.com>
2023-02-03 20:32:43 +0100

    Prueba insertar parrafo en body con JS

---
+++ Prueba.js
@@ -0,0 +1,6 @@
+let ElBody
+ElBody = document.querySelector("body")
+let MiPrimerParrafo
+MiPrimerParrafo = document.createElement("p")
+MiPrimerParrafo.innerText = "Lo que hay que hacer para poner una frase"
+ElBody.appendChild(MiPrimerParrafo)
